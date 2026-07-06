7b4a4e92f6efc479439317b56c674a2ae5bcd469
DaisukeUe <naruhodosan012@gmail.com>
2026-07-06 16:14:35 +0900

    ログイン、サインアップ

--- koshien_front/src/compornets/Login.jsx
+++ koshien_front/src/compornets/Login.jsx
@@ -2,15 +2,68 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 import LoginApp from "./login/LoginApp";
 import qtaImage from "/public/Signal-2.png";
+import { app, auth } from "../../firebase/firebase.config";
+
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
 function Login() {
   const navigate = useNavigate();
-  const handleLoginI = () => {
-    navigate("/dashboard");
+  // 認証する利用者IDの入力の値を管理するstate
+  const [certId, setCertId] = useState("");
+  // 認証するパスワード利用者IDの入力の値を管理するstate
+  const [certPw, setCertPw] = useState("");
+  // これで 'auth' オブジェクトを使って認証機能を利用できます
+
+  console.log("Firebase Authサービスが取得されました:", auth);
+  const handleSetupSubmit = (e) => {
+    e.preventDefault();
+    if (certId === null || certId === "") {
+      alert("入力要件が合致していません。");
+    } else {
+      console.log(" id=" + certId + " pw=" + certPw);
+      console.log("ここにFirebaseへの利用者認証を行うロジックを入れる");
+      let user = "";
+      let token = "";
+
+      signInWithEmailAndPassword(auth, certId, certPw)
+        .then((userCredential) => {
+          // ログインが成功した場合
+          user = userCredential.user;
+          console.log("firebaseログイン成功:", user);
+          localStorage.setItem("user_id", user.uid);
+          token = user.getIdToken();
+        })
+        .then(() => {
+          const response = fetch("http://localhost:8080/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+              uid: user.uid,
+              email: user.email,
+              name: "FrontTestUser1",
+              permission: false,
+            }),
+          });
+          console.log("アプリログイン成功:", user);
+          navigate("/dashboard");
+        })
+        .catch((error) => {
+          // エラーが発生した場合
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.error("ログインエラー:", errorCode, errorMessage);
+          // エラーコードに基づいてエラーハンドリングを行う
+        });
+    }
   };
-  const handleLoginS = () => {
-    navigate("/dashboard_s");
-  };
+
   const handleCreate = () => {
     navigate("create");
   };
@@ -29,21 +82,25 @@ function Login() {
           </div>
           <div>
             アドレス
-            <input type="text" placeholder="example@toyota.co.jp" />
+            <input
+              type="text"
+              placeholder="example@toyota.co.jp"
+              onChange={(e) => setCertId(e.target.value)}
+            />
           </div>
           <div>
             <div>Password</div>
 
-            <input type="password" placeholder="password" />
+            <input
+              type="password"
+              required
+              placeholder="password"
+              onChange={(e) => setCertPw(e.target.value)}
+            />
           </div>
           <div>
-            <button className="loginBtn" onClick={handleLoginI}>
+            <button className="loginBtn" onClick={handleSetupSubmit}>
               講師ログイン
-            </button>
-          </div>
-          <div>
-            <button className="loginBtn" onClick={handleLoginS}>
-              受講生ログイン
             </button>
           </div>
           <div>
