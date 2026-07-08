34bbaef55a9b47b94553f9ef0d63d681fe2d49c7
YumaInagaki12 <user@RNTM1340.local>
2026-07-08 16:57:54 +0900

    三項演算子に変更

--- koshien_front/src/compornets/dashboard/Schedule/ScheduleRight.jsx
+++ koshien_front/src/compornets/dashboard/Schedule/ScheduleRight.jsx
@@ -4,8 +4,6 @@ import { atomUserMode } from "../atoms";
 import { useAtom } from "jotai";
 //userには講師かユーザかどっちかが入る
 function ScheduleRight(data) {
-  const [side, setSide] = useState("");
-  const [info, setInfo] = useState("");
   const [userMode, setUserMode] = useAtom(atomUserMode);
 
   async function list() {
@@ -21,29 +19,20 @@ function ScheduleRight(data) {
         </div>
       </li>;
     });
-
     //trueが受講生falseが講師
   }
-  useEffect(() => {
-    if (userMode === false) {
-      setSide("実施済みの講義一覧");
-      setInfo("黄色はまとめができている講義だよ");
-      console.log("講師");
-    } else {
-      setSide("受講済みの講義一覧");
-      setInfo("お疲れ様！");
-      console.log("受講生");
-    }
-  }, [userMode]);
 
-  console.log(userMode);
   return (
     <>
       <div className="containerr">
         <div id="title">
-          <p id="text">{side}</p>
+          <p id="text">
+            {userMode ? "受講済みの講義一覧" : "実施済みの講義一覧"}
+          </p>
           <div id="info">
-            <p id="infoText">{info}</p>
+            <p id="infoText">
+              {userMode ? "お疲れ様！" : "黄色はまとめができている講義だよ"}
+            </p>
           </div>
           <img src="Q_ta_wao.png" alt="Q太" id="Q_ta_wao"></img>
         </div>
